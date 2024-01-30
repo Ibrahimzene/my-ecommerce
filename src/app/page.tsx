1d587e47952b70d69ec29632c874730e7693af3a
Ibrahimzene <131875836+Ibrahimzene@users.noreply.github.com>
2024-01-30 11:05:54 +0100

    Refactor page.tsx to import product data from db.json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import Image from "next/image";
+import db from "@/data/db.json";
+
+const products = db.products;
 
 export default function Home() {
-  return (
-    <main>
-      <h1 className="text-2xl mb-3">E-Commerce Site</h1>
-      <p>Welcome.</p>
-    </main>
-  );
+	return (
+		<main>
+			<h1 className="text-2xl mb-3">E-Commerce Site</h1>
+			<p>Welcome.</p>
+		</main>
+	);
 }
